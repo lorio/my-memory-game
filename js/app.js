694b3995ced46e111ed44cf9f655a66c134aee93
lorio <lori@loriortiz.com>
2018-06-04 14:29:39 -0400

    Move variables to top

--- js/app.js
+++ js/app.js
@@ -1,20 +1,8 @@
 //help from https://github.com/RJGrunau/fend-project-memory-game/blob/master/js/app.js#L18 and Mike Wales video.
 
 //to start //////////////////////
-const cards = document.querySelectorAll('.card');
+let cards = document.querySelectorAll('.card');
 const deck = document.querySelector('.deck');
-function initGame() {
-  
-// loop through each card and create its HTML ///////
-  let cardHTML = pix.map(function (card) {
-    return generateCards(card);
-  });  // Display the cards on the page
-// add each card's HTML to the page ////////////////
-    deck.innerHTML = cardHTML.join('');    
-}
-/*
- * Create a list that holds all of your cards
- */
 let pix = ['fa-paper-plane-o', 'fa-paper-plane-o',
           'fa-diamond', 'fa-diamond',
           'fa-anchor', 'fa-anchor',
@@ -24,14 +12,27 @@ let pix = ['fa-paper-plane-o', 'fa-paper-plane-o',
           'fa-bicycle', 'fa-bicycle',
           'fa-bomb', 'fa-bomb'
  ];
+// create an empty list of open cards
+let openCards = [];
+let moves = 1;
+const movesCounter = document.querySelectorAll('.moves');
 
+function initGame() {
+// loop through each card and create its HTML ///////
+  let cardHTML = pix.map(function (card) {
+    return generateCards(card);
+  });  
+  // Display the cards on the page
+// add each card's HTML to the page ////////////////
+    deck.innerHTML = cardHTML.join('');    
+}
+ /////* Create a list that holds all of your cards *//////////
  function generateCards(card) {
   return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
  }
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(pix) {
     var currentIndex = pix.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -39,37 +40,28 @@ function shuffle(pix) {
         pix[currentIndex] = pix[randomIndex];
         pix[randomIndex] = temporaryValue;
     }
-
     return pix;
 }
-
-// create an empty list of open cards
-let openCards = [];
-let moves = 1;
-const movesCounter = document.querySelectorAll('.moves');
 //start game ///////////////////////////////////
 
+/*card {
+  addEventListener(click, function(e){
+    card.classList.add(open, show);
+  });
+}   */
 
-initGame();  
-  
-//  }; 
-    cards.forEach(function(card){
-      card.addEventListener('click', function(e){
-        openCards.push(card);
-        card.classList.add('open', 'show');
-        console.log(e);
-        //cardMatch(card);
-      });
-    });
-  
 
-      /*if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {*/
-    
-      
-/*        
-      };
-    });
-});  */  
+cards.forEach(function(card){
+  card.addEventListener('click', function(e){
+    openCards.push(card);
+    cards.classList.add('open', 'show');
+    console.log(e);
+    //cardMatch(card);
+  });
+});
+initGame();   
+   /*if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {*/   
+/*          
 /*if match*/
       if (openCards.length == 2) {
         if (openCards[0].dataset.card == openCards[1].dataset.card) {
